4dd46cc83d406ef1874d1922e882734f286751b4
Vahan <vahan.khanikyan@nauto.com>
2020-04-17 15:16:06 +0200

    Add apple discount test case

--- src/test/java/com/ubs/vahan/task/shoppingbasket/ShoppingBasketTest.java
+++ src/test/java/com/ubs/vahan/task/shoppingbasket/ShoppingBasketTest.java
@@ -1,5 +1,6 @@
 package com.ubs.vahan.task.shoppingbasket;
 
+import com.ubs.vahan.task.shoppingbasket.strategy.DiscountStrategyApples;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -22,4 +23,12 @@ public class ShoppingBasketTest {
         shoppingbasket.addProduct("Orange", 1.2);
         assertEquals(ShoppingBasket.PRODUCT_PRICE_MAP.get("Orange").multiply(BigDecimal.valueOf(2.4)), shoppingbasket.calculateTotal());
     }
+
+    @Test
+    public void calculateTotal_AppleDiscount() {
+        ShoppingBasket shoppingbasket = new ShoppingBasket(new DiscountStrategyApples());
+        shoppingbasket.addProduct("Apple", 2.5);
+        assertEquals(ShoppingBasket.PRODUCT_PRICE_MAP.get("Apple").multiply(BigDecimal.valueOf(2.5/2)), shoppingbasket.calculateTotal());
+    }
+
 }
