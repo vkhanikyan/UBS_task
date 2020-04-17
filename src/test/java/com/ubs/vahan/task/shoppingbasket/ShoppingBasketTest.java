db8f8bb62f84948f1420856460ef9800ab2357e4
Vahan <vahan.khanikyan@nauto.com>
2020-04-17 14:55:39 +0200

    Add product quantity calculation update test

--- src/test/java/com/ubs/vahan/task/shoppingbasket/ShoppingBasketTest.java
+++ src/test/java/com/ubs/vahan/task/shoppingbasket/ShoppingBasketTest.java
@@ -14,4 +14,12 @@ public class ShoppingBasketTest {
         shoppingbasket.addProduct("Orange", 1.2);
         assertEquals(ShoppingBasket.PRODUCT_PRICE_MAP.get("Orange").multiply(BigDecimal.valueOf(1.2)), shoppingbasket.calculateTotal());
     }
+
+    @Test
+    public void calculateTotal_2TimesTheSameItemCase() {
+        ShoppingBasket shoppingbasket = new ShoppingBasket();
+        shoppingbasket.addProduct("Orange", 1.2);
+        shoppingbasket.addProduct("Orange", 1.2);
+        assertEquals(ShoppingBasket.PRODUCT_PRICE_MAP.get("Orange").multiply(BigDecimal.valueOf(2.4)), shoppingbasket.calculateTotal());
+    }
 }
